a290a709b8b1a7ef49cdc6a4c4d0281cb9a47baf
Naman-12911 <namangarg820@gmail.com>
2023-10-11 20:50:48 +0530

    autentication part is done

---
+++ account/serializer.py
@@ -0,0 +1,105 @@
+from .models import User
+from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib import auth
+from rest_framework_simplejwt.tokens import RefreshToken
+
+# register serializers
+class UserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name','phone_no','email','password','employee','client']
+        extra_kwargs = {
+            'password' :{'write_only':True}  #  to does not return password in api ## postman
+        }
+    # convert password to hash key
+    def create(self, validated_data):
+        password = validated_data.pop('password',None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        if len(password) <6:
+            raise serializers.ValidationError("entre strong password")
+        instance.save()
+        return instance
+# login serializers
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=3)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    tokens = serializers.SerializerMethodField()
+    client = serializers.BooleanField(read_only=True)
+    employee = serializers.BooleanField(read_only=True)
+
+   
+    def get_tokens(self, obj):
+        user = User.objects.get(email=obj['email'])
+        tokens_data = {
+            'refresh': user.tokens()['refresh'],
+            'access': user.tokens()['access']
+        }
+
+    # Include 'client' and 'employee' only if they are True
+        if user.client:
+            tokens_data['fitness'] = user.client
+        if user.employee:
+            tokens_data['employee'] = user.employee
+
+        return tokens_data
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'tokens', 'client', 'employee']
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        filtered_user_by_email = User.objects.filter(email=email)
+        user = auth.authenticate(email=email, password=password)
+
+        if filtered_user_by_email.exists() and filtered_user_by_email[0].auth_provider != 'email':
+            raise AuthenticationFailed(
+                detail='Please continue your login using ' + filtered_user_by_email[0].auth_provider)
+        if not user:
+            raise AuthenticationFailed('Invalid credentials, try again')
+        
+        if not user.is_verified:
+                raise AuthenticationFailed('Email is not verified')
+
+        return {
+            'email': user.email,
+            'tokens': user.tokens,
+         
+        }
+
+        return super().validate(attrs)
+
+
+# email verification
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+
+# logout apis
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+    default_error_message = {
+        'bad_token': ('Token is expired or invalid')
+    }
+
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
+
+    def save(self, **kwargs):
+        RefreshToken(self.token).blacklist()
+
+
+
+# password reset api
+class PasswordResetSerializer(serializers.Serializer):
+    email = serializers.EmailField()
---
+++ account/views.py
@@ -0,0 +1,119 @@
+from .serializer import UserSerializers,UserLoginSerializer,PasswordResetSerializer,EmailVerificationSerializer,LogoutSerializer
+from rest_framework.response import Response
+from .models import User # import models
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework import status
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
+from django.conf import settings
+import jwt
+from .utils import Utils
+
+# register the user
+class Register(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializers
+    def post(self, request):
+        user = request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+        token = RefreshToken.for_user(user).access_token
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://'+current_site+relativeLink+"?token="+str(token)
+        email_body = 'Hi '+user.email + \
+            ' Use the link below to verify your email \n' + absurl
+        data = {'email_body': email_body, 'to_email': user.email,
+                'email_subject': 'Verify your email'}
+
+        Utils.send_email(data)
+        return Response(user_data, status=status.HTTP_201_CREATED)
+    
+# login apis
+class Login(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+
+# verify the mail that send in the mail box
+class VerifyEmail(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = EmailVerificationSerializer
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"],type=jwt)
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+             return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+class userData(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        serializer = UserSerializers(self.request.user)
+        return Response(serializer.data)
+
+# update the user details
+class update_profile(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializers
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(request.user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+
+class PasswordResetAPIView(generics.GenericAPIView):
+    serializer_class = PasswordResetSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        form = PasswordResetForm(serializer.validated_data)
+        if form.is_valid():
+            form.save(request=request)
+            return Response({'detail': 'Password reset email has been sent.'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'detail': 'Failed to send password reset email.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = LogoutSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            serializer.save()
+            return Response({"detail": "Successfully logged out."}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"detail": "Failed to log out. {}".format(str(e))}, status=status.HTTP_400_BAD_REQUEST)
