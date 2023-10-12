c00141bab1029a4f1be9e90c9d50d7ebc5a6ad2d
Naman-12911 <namangarg820@gmail.com>
2023-10-12 20:37:07 +0530

    remove error static files

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -235,9 +235,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'btre/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'job_portal/static')
+# ]
 
 # MEDIA FOLDER SETTINGS
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
